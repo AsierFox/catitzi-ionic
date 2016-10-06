e699416fdcfbab874614dfc300f8ccd2ef5ce481
asier <asier@ideable.net>
2016-10-06 13:53:13 +0200

    Get cats & Quotes

--- www/js/constants.js
+++ www/js/constants.js
@@ -1,2 +1,2 @@
-var CATS_API = "http://thecatapi.com/api/images/get?api_key=MTIzNjQy&format=xml&results_per_page=1";
+var CATS_API = "http://thecatapi.com/api/images/get?api_key=MTIzNjQy&format=xml&results_per_page=6";
 var QUOTES_API = "https://crossorigin.me/http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en";
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,25 +1,47 @@
 angular.module('controllers', [])
 
-.controller('DashCtrl', function($scope) {})
+.controller('DashCtrl', function($scope) {
+})
 
-.controller('ChatsCtrl', function($scope, Chats) {
+.controller('ChatsCtrl', function($scope, $http, xml2jsonService) {
 
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
+    $scope.cats = [];
+    $scope.getCats = getCats;
 
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  };
+    $scope.$on("$ionicView.enter", function() {
+        getCats();
+    });
 
+    function getCats() {
+        $http
+            .get(CATS_API)
+            .then(function(res) {
+                var xml = xml2jsonService.parseXML(res.data);
+                var cats = JSON.parse(xml2jsonService.xml2json(xml, ''));
+
+                $scope.cats = cats.response.data.images.image;
+            });
+    }
 })
 
 .controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
+    $scope.chat = Chats.get($stateParams.chatId);
 })
 
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
+.controller('AccountCtrl', function($scope, $http) {
+
+    $scope.quote = {};
+    $scope.getQuote = getQuote;
+
+    $scope.$on("$ionicView.enter", function() {
+        getQuote();
+    });
+
+    function getQuote() {
+        $http
+            .get(QUOTES_API)
+            .then(function(res) {
+                $scope.quote = res.data;
+            });
+    }
 });
